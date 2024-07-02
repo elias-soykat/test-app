3d2f8645908d1591f71af099e0ae3dfcf0b764a4
Elias Soykat <eliasmd624@gmail.com>
2024-07-02 22:53:53 +0600

    fix : dokkher gan

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,6 @@ import Image from "next/image";
 import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    throw new Error("Throw an error");
-  }, []);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
